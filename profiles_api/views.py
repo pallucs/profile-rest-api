22d58f47b19263d696a3a93a8bbf2b651fe734b6
pallucs <palludallu91@gmail.com>
2022-05-26 15:00:05 +0900

    added profiles viewswet and authentication

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,8 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response 
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+
 
 from profiles_api import serializers
+from profiles_api import models, permissions
 
 
 class HelloApiView(APIView):
@@ -95,3 +98,13 @@ class HelloViewSet(viewsets.ViewSet):
     
     def destroy(self, request, pk=None):
         return Response({'http_method': 'DELETE'})
+        
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """handle creating and updating profles"""
+    
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    
+     
